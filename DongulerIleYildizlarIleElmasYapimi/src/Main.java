9a8b46cea0e415f3ce7243c89e6846ad949d2b81
sametacay <sametacay034@gmail.com>
2024-05-07 23:44:40 +0300

    Add files via upload

---
+++ DongulerIleYildizlarIleElmasYapimi/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sayi;
+        System.out.print("Kaç satırlık yıldız işlemi istiyorsunuz ? : ");
+        sayi = scan.nextInt();
+        for (int i = 1; i <= sayi; i++) {
+            for (int k = 1; k <= (sayi - i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = sayi - 1; i >= 1; i--) {
+            for (int k=sayi; k>i; k--) {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=(2 * i)-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
